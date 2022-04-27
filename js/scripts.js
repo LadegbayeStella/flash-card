ee5f2d114b8382f7aa31ed3095a6c69aa999c7a4
ladegbaye Stella <Ladegbayeewatomi50@gmail.com>
2022-04-27 15:30:01 +0100

    final commit

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+
+jQuery(document).ready(function () {
+  $("#ik").click(function () {
+    $(".ni").fadeToggle(300);
+  });
+
+  $("#po").click(function () {
+    $(".si").fadeToggle(400)
+  });
+
+
+  $("#ho").click(function () {
+    $(".pe").fadeToggle(400)
+  });
+
+
+  $("#co").click(function () {
+    $(".me").fadeToggle(400)
+  });
+
+
+
+
+
+
+
+
+
+});
+
+
+
+jQuery(document).ready(function () {
+
+  $("#mi").click(function () {
+    $(".lo").fadeToggle(300);
+  });
+
+  $("#ira").click(function () {
+    $(".wo").fadeToggle(400)
+  });
+
+
+  $("#fai").click(function () {
+    $(".th").fadeToggle(400)
+  });
+
+
+  $("#go").click(function () {
+    $(".es").fadeToggle(400)
+  });
+});
